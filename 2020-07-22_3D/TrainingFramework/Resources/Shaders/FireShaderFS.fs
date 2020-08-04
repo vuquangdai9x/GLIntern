cecad74580a80ed434c2bb75eee991c00216d19c
Vu Quang Dai <vuquangdai9x@gmail.com>
2020-08-04 17:28:49 +0700

    - tối ưu file FireShaderFS

--- 2020-07-22_3D/TrainingFramework/Resources/Shaders/FireShaderFS.fs
+++ 2020-07-22_3D/TrainingFramework/Resources/Shaders/FireShaderFS.fs
@@ -8,12 +8,12 @@ uniform float u_time;
 uniform float u_displMax;
 
 varying vec2 v_uv;
-
+	
 varying float v_fogDensity;
 
 void main()
 {
-	vec2 offset = (2.0 * texture2D(u_displTex, vec2(v_uv.x, fract(v_uv.y + u_time))).rg - 1.0) * u_displMax;
+	vec2 offset = (2.0 * texture2D(u_displTex, vec2(v_uv.x, v_uv.y + u_time)).rg - 1.0) * u_displMax;
 	float alpha = texture2D(u_alphaMaskTex, v_uv).r;
 	vec4 color = texture2D(u_texture, v_uv + offset);
 	color = u_fogColor * v_fogDensity + color * (1.0 - v_fogDensity);
