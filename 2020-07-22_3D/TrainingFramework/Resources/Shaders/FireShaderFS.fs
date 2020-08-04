02918e85c6e123273416f4db2d1799140da6dc19
Vu Quang Dai <vuquangdai9x@gmail.com>
2020-08-04 16:33:24 +0700

    * sửa lỗi Fire Shader không chuyển động khi đặt tilingMode là clamp

--- 2020-07-22_3D/TrainingFramework/Resources/Shaders/FireShaderFS.fs
+++ 2020-07-22_3D/TrainingFramework/Resources/Shaders/FireShaderFS.fs
@@ -13,9 +13,9 @@ varying float v_fogDensity;
 
 void main()
 {
-	vec2 offset = (2.0 * texture2D(u_displTex, vec2(v_uv.x, v_uv.y + u_time)).rg - 1.0) * u_displMax;
+	vec2 offset = (2.0 * texture2D(u_displTex, vec2(v_uv.x, fract(v_uv.y + u_time))).rg - 1.0) * u_displMax;
 	float alpha = texture2D(u_alphaMaskTex, v_uv).r;
 	vec4 color = texture2D(u_texture, v_uv + offset);
-	//color = u_fogColor * v_fogDensity + color * (1.0 - v_fogDensity);
+	color = u_fogColor * v_fogDensity + color * (1.0 - v_fogDensity);
 	gl_FragColor = color * vec4(1.0,1.0,1.0,alpha);
 }
--- 2020-07-22_3D/TrainingFramework/TrainingFramework/Texture.cpp
+++ 2020-07-22_3D/TrainingFramework/TrainingFramework/Texture.cpp
@@ -45,8 +45,6 @@ bool Texture::LoadTextureFromFile(char * filePath, TILING_MODE tilingMode)
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		break;
 	}
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
