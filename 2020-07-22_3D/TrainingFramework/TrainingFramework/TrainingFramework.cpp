aed637ac07bdbff20ff9126b674d09af9f66846c
Vu Quang Dai <vuquangdai9x@gmail.com>
2020-08-04 10:48:41 +0700

    + tạo branch mới để tối ưu lại engine structure

--- 2020-07-22_3D/TrainingFramework/TrainingFramework/TrainingFramework.cpp
+++ 2020-07-22_3D/TrainingFramework/TrainingFramework/TrainingFramework.cpp
@@ -33,7 +33,7 @@ int Init ( ESContext *esContext )
 	SceneManager::GetInstance()->Init("Datas/scene.txt");
 	InputManager::CreateInstance();
 
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(1.0f, 0.8f, 1.0f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
 	return 0;
 }
